34141281232719d5d92c13c46421180d7cd94789
mariamoraan <mariamoranluaces@gmail.com>
2024-02-13 17:36:08 +0100

    Initial Commit

---
+++ src/features/commits/ui/components/commit-form/CommitForm.tsx
@@ -0,0 +1,26 @@
+import styles from './CommitForm.module.css'
+
+export const CommitForm = () => {
+    const labels = [
+        {name: 'type', label: 'Type', type: 'text'},
+        {name: 'description', label: 'Description', type: 'text'},
+        {name: 'body', label: 'Body', type: 'text'},
+        {name: 'footer', label: 'Footer', type: 'text'},
+    ]
+    return (
+        <form className={styles.wrapper}>
+            {
+                labels.map(label => (
+                    <>
+                        <label htmlFor={label.name}>{label.label}</label>
+                        <input
+                        id={label.name}
+                        name={label.name}
+                        type={label.type}
+                        />
+                    </>
+                ))
+            }
+        </form>
+    )
+}
